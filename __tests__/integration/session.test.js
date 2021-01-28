1b45288c120106277ab76ba1c75df340c76185b6
junior.esbaltar <junior.esbaltar@gmail.com>
2021-01-28 20:13:58 -0300

    user authentic test

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,1 +1,15 @@
-it("should receive JWT token when authenticated with valid credentials", () => {});
+const { User } = require("../../src/app/models");
+
+describe("Authentication", () => {
+  it("should sum two numbers", async () => {
+    const user = await User.create({
+      name: "Júnior",
+      email: "junior@email.com",
+      password_hash: "5asd46as",
+    });
+
+    console.log(user);
+
+    expect(use.email).toBe("junior@email.com");
+  });
+});
--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,19 @@
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
+});
+
 module.exports = {
-  host: '127.0.0.1',
-  username: 'postgres',
-  password: 'root',
-  database: 'nodeauth',
-  dialect: 'postgres',
+  host: "127.0.0.1",
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT || "postgres",
+  storage: "./__tests__/database.sqlite",
   operatorsAliases: false,
   logging: false,
   define: {
     timestamps: true,
     underscored: true,
-    underscoredAll:true,
+    underscoredAll: true,
   },
-}
+};
